256c3e8cae2be6daac50649bc1abc7edec916dae
spunuru <spunuru@landmarkhealth.org>
2022-12-25 08:43:32 +0530

    Assignment 4 Implementing As per Code review comments

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -1,12 +1,12 @@
 const CardComponent=(props)=>{
-    debugger;
+    const teamMembers =props.teamMembers;
     return (
         <div id="card" className="cardClass">
-            <img src={props.teamMembers.img}/>
-            <h2>{props.teamMembers.name}</h2>
-            <h3>{props.teamMembers.company}</h3>
-            <h4>{props.teamMembers.address}</h4>
-            <h5>{props.teamMembers.designation}</h5>
+            <img src={teamMembers.img}/>
+            <h2>{teamMembers.name}</h2>
+            <h3>{teamMembers.company}</h3>
+            <h4>{teamMembers.address}</h4>
+            <h5>{teamMembers.designation}</h5>
         </div>
     )
 };
